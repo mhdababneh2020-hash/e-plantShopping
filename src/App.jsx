efcbdbcd7c353212747b2bfd33fee20acbec4ff8
mhdababneh2020-hash <mhdababneh2020@outlook.com>
2026-02-10 22:43:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+import React, { useState } from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import ProductList from "./ProductList";
+import "./App.css";
+import AboutUs from "./AboutUs";
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -15,35 +15,35 @@ function App() {
   const handleHomeClick = () => {
     setShowProductList(false);
   };
-
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+      <div className={`landing-page ${showProductList ? "fade-out" : ""}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
 
+            <button
+              className="get-started-button"
+              onClick={handleGetStartedClick}
+            >
+              Get Started
+            </button>
+          </div>
+          <div className="aboutus_container">
+            <AboutUs />
+          </div>
+        </div>
       </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList onHomeClick={handleHomeClick}/>
+      <div
+        className={`product-list-container ${showProductList ? "visible" : ""}`}
+      >
+        <ProductList onHomeClick={handleHomeClick} />
       </div>
     </div>
   );
 }
 
 export default App;
-
-
-
